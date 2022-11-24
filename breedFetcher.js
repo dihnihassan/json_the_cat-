1c7123c6b309b1ba04aab7c64a04fd43c083885d
Dihni Hassan <dihnihassan@gmail.com>
2022-11-24 16:39:50 -0500

    Edited to include edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,18 +5,27 @@ console.log(process.argv);
 
 const request = require('request');
 request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Example homepage.
 
   // body is a string
-  // console.log(typeof body);
+
+  if (error) {
+    return console.log("This is an error", error);
+  }
 
   const data = JSON.parse(body);
-  // console.log(data);
+  // console.log("Print data info", data);
+  // console.log(typeof body);
+
+  if (!data.length) {
+    return console.log("Breed does not exist");
+  }
+
+
 
   console.log(data[0].description);
-
 
 
 });
