a57293a922ca9b607fcf3cfa819cb6380836faba
Dihni Hassan <dihnihassan@gmail.com>
2022-11-24 15:55:21 -0500

    Formatted with eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,7 +4,7 @@ console.log(process.argv);
 
 
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}` , (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Example homepage.
@@ -12,10 +12,10 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}` , (err
   // body is a string
   // console.log(typeof body);
 
-const data = JSON.parse(body);
-// console.log(data);
+  const data = JSON.parse(body);
+  // console.log(data);
 
-console.log(data[0].description);
+  console.log(data[0].description);
 
 
 
@@ -24,7 +24,7 @@ console.log(data[0].description);
 
 
 // if statement inside function if breed name not found
-// return error using if statement    
-    //  if(err) {
-    //   console.error(err);
-    // }
+// return error using if statement
+// if(err) {
+// console.error(err);
+// }
